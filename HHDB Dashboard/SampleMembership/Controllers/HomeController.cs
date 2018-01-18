17d41338380d31a785bec262c80fa40378e4e098
Dustin Go <dustinvin@gmail.com>
2018-01-18 18:02:26 -0500

    Adding  Reports  Contoller  and view of Pie Chart

--- HHDB Dashboard/SampleMembership/Controllers/HomeController.cs
+++ HHDB Dashboard/SampleMembership/Controllers/HomeController.cs
@@ -31,5 +31,10 @@ namespace SampleMembership.Controllers
 
             return View();
         }
+
+        public ActionResult Chart()
+        {
+            return View();
+        }
     }
 }
---
+++ HHDB Dashboard/SampleMembership/Controllers/ReportsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMembership.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PieChart()
+        {
+            return View();
+        }
+
+        public ActionResult LineGraph()
+        {
+            return View();
+        }
+
+        public ActionResult Import()
+        {
+            return View("~/Views/import/Index.cshtml");
+        }
+
+        public ActionResult CreateGraph()
+        {
+            return View();
+        }
+    }
+}

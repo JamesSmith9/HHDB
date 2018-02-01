84e0fe5c05e98efb0948d4e765af3dcc126c5b46
Dustin Go <dustinvin@gmail.com>
2018-02-01 12:46:40 -0500

    Added some code to the reports controller. Also created a new table in our database

--- HHDB Dashboard/SampleMembership/Controllers/ReportsController.cs
+++ HHDB Dashboard/SampleMembership/Controllers/ReportsController.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SampleMembership.Models;
 using System.Web.Mvc;
 
 namespace SampleMembership.Controllers
 {
     public class ReportsController : Controller
     {
+
+        private HHDBEntities db = new HHDBEntities();
+
+        public class Summary
+        {
+            public double Item { get; set; }
+            public string Value { get; set; }
+        }
+
+        public JsonResult QuestionSummary()
+        {
+            List<Summary> lstSummary = new List<Summary>();
+            Summary summary = new Summary();
+            
+            return Json(lstSummary.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
         // GET: Reports
         public ActionResult Index()
         {

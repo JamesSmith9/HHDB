ea8086079baae06d7c5ae20036010ff6cd34ba2f
Dustin Go <dustinvin@gmail.com>
2018-02-15 17:34:03 -0500

    Updated Code but still haven't been able to connect highcharts to the database

--- HHDB Dashboard/SampleMembership/Controllers/ReportsController.cs
+++ HHDB Dashboard/SampleMembership/Controllers/ReportsController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using SampleMembership.Models;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace SampleMembership.Controllers
 {
@@ -12,20 +15,43 @@ namespace SampleMembership.Controllers
 
         private HHDBEntities db = new HHDBEntities();
 
+        public static DataTable GetQuestionSummary()
+        {
+            DataTable dt = new DataTable("ChartSummary");
+            string query = "Select QuestionAns From [dbo].[Answers_int]";
+            string connString = ConfigurationManager.ConnectionStrings["SampleMembershipDB"].ConnectionString;
+            SqlConnection con = new SqlConnection(connString);
+            //con.ConnectionString = "Data Source=.;" + "Initial Catalog=Transport;" + "Persist Security Info=True;";
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = query;
+            //cmd.Connection = con;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            con.Open();
+            da.Fill(dt);
+            con.Close();
+            return dt;
+
+        }
         public class Summary
         {
             public double Item { get; set; }
             public string Value { get; set; }
         }
 
-        public JsonResult QuestionSummary()
+        public JsonResult ChartSummary()
         {
             List<Summary> lstSummary = new List<Summary>();
-            Summary summary = new Summary();
-            
+
+            foreach (DataRow dr in GetQuestionSummary().Rows)
+            {
+                Summary summary = new Summary();
+                summary.Value = dr[0].ToString().Trim();
+                summary.Item = Convert.ToDouble(dr[1]);
+                lstSummary.Add(summary);
+
+            }
             return Json(lstSummary.ToList(), JsonRequestBehavior.AllowGet);
         }
-
         // GET: Reports
         public ActionResult Index()
         {

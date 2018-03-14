f977e5493a68778fee9dce04748a8b45db1cee5f
Elizabeth Thompson <lizthompson01234@gmail.com>
2018-03-14 08:50:19 -0400

    Highcharts Configuration & Web API

---
+++ HHDB Dashboard/SampleMembership/App_Start/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Http;
+
+namespace SampleMembership
+{
+	using System.Web.Http;
+
+	class WebApiConfig
+	{
+		public static void Register(HttpConfiguration configuration)
+		{
+			configuration.MapHttpAttributeRoutes();
+
+			configuration.Routes.MapHttpRoute("API Default", "api/{controller}/{id}",
+				new { id = RouteParameter.Optional });
+		}
+	}
+}
--- HHDB Dashboard/SampleMembership/Global.asax.cs
+++ HHDB Dashboard/SampleMembership/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace SampleMembership
 {
@@ -12,10 +13,12 @@ namespace SampleMembership
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+			AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+			WebApiConfig.Register(GlobalConfiguration.Configuration);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }

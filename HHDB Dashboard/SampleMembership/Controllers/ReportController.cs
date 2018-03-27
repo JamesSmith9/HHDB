36c96ef019bbfd73fb52c4374ad1a5b7b6fdc802
JamesSmith9 <31371095+JamesSmith9@users.noreply.github.com>
2018-03-26 21:22:02 -0400

    Some reports stuff added

--- HHDB Dashboard/SampleMembership/Controllers/ReportController.cs
+++ HHDB Dashboard/SampleMembership/Controllers/ReportController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SampleMembership.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,31 @@ namespace SampleMembership.Controllers
 {
     public class ReportController : Controller
     {
+        private HHDBEntities db = new HHDBEntities();
+
         // GET: Report
-        public ActionResult Index()
+        public ActionResult Index() => View(db.Surveys.ToList());
+
+
+
+        public ActionResult ReportForm(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Survey survey = db.Surveys.Find(id);
+
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Gets Active and Inactive Survey Questions
+            survey.SurveyQuestions = db.SurveyXQuestions.Where(x => x.SurveyID == survey.SurveyID)
+                .Select(x => x.Question).ToList();
+
+            return View(survey);
         }
     }
 }

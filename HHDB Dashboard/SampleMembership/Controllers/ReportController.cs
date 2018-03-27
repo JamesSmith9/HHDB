30d0413aa2405afefce3138405094306f35ef242
Dustin Go <dustinvin@gmail.com>
2018-03-27 17:14:55 -0400

    Revert "Merge branch 'master' of https://github.com/JamesSmith9/HHDB"
    This reverts commit f791908fc50028ee4da73096759b44dd046bba88.

--- HHDB Dashboard/SampleMembership/Controllers/ReportController.cs
+++ HHDB Dashboard/SampleMembership/Controllers/ReportController.cs
@@ -1,8 +1,6 @@
-﻿using SampleMembership.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,31 +8,10 @@ namespace SampleMembership.Controllers
 {
     public class ReportController : Controller
     {
-        private HHDBEntities db = new HHDBEntities();
-
         // GET: Report
-        public ActionResult Index() => View(db.Surveys.ToList());
-
-
-
-        public ActionResult ReportForm(int? id)
+        public ActionResult Index()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Survey survey = db.Surveys.Find(id);
-
-            if (survey == null)
-            {
-                return HttpNotFound();
-            }
-
-            // Gets Active and Inactive Survey Questions
-            survey.SurveyQuestions = db.SurveyXQuestions.Where(x => x.SurveyID == survey.SurveyID)
-                .Select(x => x.Question).ToList();
-
-            return View(survey);
+            return View();
         }
     }
 }

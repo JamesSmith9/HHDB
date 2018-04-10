8cd305560af3391a3187c0fe27c42dbcd6a1f893
Elizabeth Thompson <lizthompson01234@gmail.com>
2018-04-10 10:53:19 -0400

    Highcharts

--- HHDB Dashboard/SampleMembership/Controllers/ReportsController.cs
+++ HHDB Dashboard/SampleMembership/Controllers/ReportsController.cs
@@ -17,48 +17,13 @@ namespace SampleMembership.Controllers
 
         private HHDBEntities db = new HHDBEntities();
 
-        public static DataTable GetQuestionSummary()
-        {
-            DataTable dt = new DataTable("ChartSummary");
-            string query = "Select QuestionAns From [dbo].[Answers_int]";
-            string connString = ConfigurationManager.ConnectionStrings["SampleMembershipDB"].ConnectionString;
-            SqlConnection con = new SqlConnection(connString);
-            //con.ConnectionString = "Data Source=.;" + "Initial Catalog=Transport;" + "Persist Security Info=True;";
-            SqlCommand cmd = new SqlCommand();
-            cmd.CommandText = query;
-            //cmd.Connection = con;
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            con.Open();
-            da.Fill(dt);
-            con.Close();
-            return dt;
 
-        }
-        public class Summary
+        public ActionResult Index()
         {
-            public double Item { get; set; }
-            public string Value { get; set; }
+            return View(db.Surveys.ToList());
         }
 
-        public JsonResult ChartSummary()
-        {
-            List<Summary> lstSummary = new List<Summary>();
-
-            foreach (DataRow dr in GetQuestionSummary().Rows)
-            {
-                Summary summary = new Summary();
-                summary.Value = dr[0].ToString().Trim();
-                summary.Item = Convert.ToDouble(dr[1]);
-                lstSummary.Add(summary);
-
-            }
-            return Json(lstSummary.ToList(), JsonRequestBehavior.AllowGet);
-        }
-        // GET: Reports
-        public ActionResult Index() => View(db.Surveys.ToList());
-
-
-        public ActionResult ReportForm(int? id)
+        public ActionResult Report(int? id)
         {
             if (id == null)
             {
@@ -76,12 +41,6 @@ namespace SampleMembership.Controllers
                 .Select(x => x.Question).ToList();
 
             return View(survey);
-        }
-
-
-        public ActionResult PieChart()
-        {
-            return View();
         }
 
     }
